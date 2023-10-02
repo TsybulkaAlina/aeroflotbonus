4779761118323d9d5d8bada0dd347d1d14dc5432
Alina Tibulca <tibulkalina@mail.ru>
2023-10-02 23:57:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 14820;
+        int y = x / 20;
+        System.out.println("Количество начисленных миль:" + y);
+        //Количество начисленных миль:741
+    }
+}
